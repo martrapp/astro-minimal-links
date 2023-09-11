7d72548dd0d5b99e94674cfde65b3f8391175082
ergberg <ergberg@ergberg.tk>
2023-09-11 09:29:39 +0200

    merged tmp

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	experimental: {
-		viewTransitions: true,
-	},
 	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
 });
