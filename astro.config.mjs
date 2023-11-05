a6435e1aa614b008bb41d232cd1e89b67c7647e6
Martin Trapp <94928215+martrapp@users.noreply.github.com>
2023-11-05 12:38:24 +0100

    add detail animation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import react from "@astrojs/react";
+import { defineConfig } from "astro/config";
 
-import sitemap from '@astrojs/sitemap';
+import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), sitemap()],
   redirects: {
     "/redirect-page1": "http://google.com/",
     "/redirect-page2": "/page2",
     "/redirect-page3": "http://google.com/",
-    "/redirect-page4": "/page4",
+    "/redirect-page4": "/page4"
   },
+  integrations: [react(), vue()]
 });
